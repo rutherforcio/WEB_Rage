174f3e28f4c4aa2e131eebae945c0185f88a5633
rutherforcio <68070150+rutherforcio@users.noreply.github.com>
2021-08-25 12:51:13 +0200

    first commit

---
+++ rage/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+# from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+#from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+
+
+app = Flask(__name__)
+
+
+mysql = MySQL()
+#bcrypt = Bcrypt()
+jwt = JWTManager()
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'rage'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['JWT_SECRET_KEY'] = 'secret'
+
+mysql.init_app(app)
+#bcrypt.init_app(app)
+jwt.init_app(app)
+
+#from baitu.ficherosPython.ventas import ventas
+#from baitu.ficherosPython.usuarios import users
+
+#app.register_blueprint(ventas)
+#app.register_blueprint(users)
+
+CORS()
+
+def create_app():
+    global app
+    return app
